ba7225af379c1916d0787a74625d2573f4e1604a
AlinaOlenaAlya <vakhrusheva-alinka@mail.ru>
2024-06-10 14:18:29 +0400

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,5 @@
+completed_hw = 12
+hours = 1.5
+course_name = 'Python'
+time_per_task = hours/completed_hw
+print('Курс:',course_name,',всего задач:' , completed_hw, ',среднее время выполнения:', time_per_task)
