30d9a9591198e4f8113f8c4922dceb9b5644c217
AlinaOlenaAlya <vakhrusheva-alinka@mail.ru>
2024-06-10 15:24:08 +0400

    Add files via upload

---
+++ hw4.py
@@ -0,0 +1,8 @@
+my_string = input('Какая погода на улице?: ')
+print('Число символов:', len(my_string))
+my_string = 'мне 19 лет'
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
