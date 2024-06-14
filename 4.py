c6c7be6b566489b6496a156ec2bc5fd5962e6fe4
AlinaOlenaAlya <vakhrusheva-alinka@mail.ru>
2024-06-14 21:13:01 +0400

    Add files via upload

--- 4.py
+++ 4.py
@@ -2,7 +2,7 @@ name = 'Alina'
 print('Name:' , name)
 age = 20
 print('Age:' , age)
-new_age = age = 30
-print('New Age:' , age)
+new_age = age + 10
+print('New Age:' , new_age)
 is_student = True
 print('Is Student:', is_student)
