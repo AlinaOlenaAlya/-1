51f458e75f40172335801861fc2739763ee22dae
AlinaOlenaAlya <vakhrusheva-alinka@mail.ru>
2024-06-14 21:07:14 +0400

    Add files via upload

---
+++ 125.py
@@ -0,0 +1,81 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+first = 0
+while len(my_list) < 14:
+    if my_list[0] > first:
+        print(my_list[0])
+    elif my_list[0] == first:
+        pass
+    elif my_list[0] < first:
+        break
+    if my_list[1] > first:
+        print(my_list[1])
+    elif my_list[1] == first:
+        pass
+    elif my_list[1] < first:
+        break
+    if my_list[2] > first:
+        print(my_list[2])
+    elif my_list[2] == first:
+        pass
+    elif my_list[2] < first:
+        break
+    if my_list[3] > first:
+        print(my_list[3])
+    elif my_list[3] == first:
+        pass
+    elif my_list[3] < first:
+        break
+    if my_list[4] > first:
+        print(my_list[4])
+    elif my_list[4] == first:
+        pass
+    elif my_list[4] < first:
+        break
+    if my_list[5] > first:
+        print(my_list[5])
+    elif my_list[5] == first:
+        pass
+    elif my_list[5] < first:
+        break
+    if my_list[6] > first:
+        print(my_list[6])
+    elif my_list[6] == first:
+        pass
+    elif my_list[6] < first:
+        break
+    if my_list[7] > first:
+        print(my_list[7])
+    elif my_list[7] == first:
+        pass
+    elif my_list[7] < first:
+        break
+    if my_list[8] > first:
+        print(my_list[8])
+    elif my_list[8] == first:
+        pass
+    elif my_list[8] < first:
+        break
+    if my_list[9] > first:
+        print(my_list[9])
+    elif my_list[9] == first:
+        pass
+    elif my_list[9] < first:
+        break
+    if my_list[10] > first:
+        print(my_list[10])
+    elif my_list[10] == first:
+        pass
+    elif my_list[10] < first:
+        break
+    if my_list[11] > first:
+        print(my_list[11])
+    elif my_list[11] == first:
+        pass
+    elif my_list[11] < first:
+        break
+    if my_list[12] > first:
+        print(my_list[12])
+    elif my_list[12] == first:
+        pass
+    elif my_list[12] < first:
+        break
